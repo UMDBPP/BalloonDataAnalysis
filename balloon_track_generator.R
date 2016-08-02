69ba549a8ac49ccf28b41b2c2cd0bbc4fa63c41f
Zachary Burnett <zrb@umd.edu>
2016-08-02 14:46:22 -0400

    more documentation

--- balloon_track_generator.R
+++ balloon_track_generator.R
@@ -3,10 +3,21 @@
 
 library(ggmap)
 
-data_file <- "NS57_parsedPackets.txt"
+launch <- "NS57"
+data_file <- paste(launch, "/", launch, "_parsedPackets.txt", sep = "")
 
+# 3 is world, 10 is city, 21 is street. May have to fine tune this variable to get good map
+zoom_level <- 10
+
+# read csv file
 data <- read.csv(data_file)
-# get base map using bounding box coordinates
-map <- get_map(location = c(min(data$Lon), min(data$Lat), max(data$Lon), max(data$Lat)), zoom = 10, maptype = "terrain")
-# plot points on map and render to plot viewer
+
+# get underlying terrain map using mean coordinates
+map <- get_map(location = c(lon = mean(data$Lon), lat = mean(data$Lat)), zoom = zoom_level, maptype = "terrain")
+
+# plot points onto map and render to plot viewer
 ggmap(map) + geom_point(data = as.data.frame(cbind(lon = data$Lon,lat = data$Lat)), aes(x = lon, y = lat, fill = data$Alt.ft.), size = 3, shape = 21)
+
+# save plot image to PNG file (NSXX_map.png)
+dev.copy(png, paste(launch, "map.png", sep = "_"))
+dev.off()
