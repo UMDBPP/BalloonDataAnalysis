d885ab702474507ba7dc5e6f49fc7a827457d1b7
Zachary Burnett <zrb@umd.edu>
2017-02-06 13:33:58 -0500

    first pass of changes

--- R/joinData_interpolate.R
+++ R/joinData_interpolate.R
@@ -4,20 +4,17 @@
 #' @param data_1 First dataset.
 #' @param data_2 Second dataset.
 #' @param by Key with which to join datasets.
-#' @param interpolate Wether to interpolate data. Defaults to TRUE.
 #' @export
 #' @importFrom zoo na.approx
 #' @importFrom zoo na.fill
 #' @examples
-#' joined_data <- joinData_interpolate(NS57_LINK_TLM, NS57_IRENE, interpolate = FALSE)
-#' joined_data_interpolated <- joinData_interpolate(NS57_LINK_TLM, NS57_IRENE)
+#' interpolated_dataset <- merge.interpolate(NS57_LINK_TLM, NS57_IRENE)
 #'
 
-joinData_interpolate <-
+merge.interpolate <-
     function(data_1,
              data_2,
-             by,
-             interpolate = TRUE)
+             by)
     {
         # outer join tables by key
         joined_data <-
@@ -29,25 +26,22 @@ joinData_interpolate <-
             )
 
         # replace NA values with interpolated values using zoo package
-        if (interpolate)
+        if (any(is.na(joined_data)))
         {
-            if (any(is.na(joined_data)))
-            {
-                data_types <- lapply(joined_data, class)
+            data_types <- lapply(joined_data, class)
 
-                for (key in by)
+            for (key in by)
+            {
+                for (column in colnames(joined_data))
                 {
-                    for (column in colnames(joined_data))
+                    if ("numeric" %in% data_types[[column]] |
+                        "integer" %in% data_types[[column]])
                     {
-                        if ("numeric" %in% data_types[[column]] |
-                            "integer" %in% data_types[[column]])
-                        {
-                            joined_data[[column]] <-
-                                zoo::na.fill(
-                                    zoo::na.approx(joined_data[[column]], joined_data[[key]], na.rm = FALSE),
-                                    "extend"
-                                )
-                        }
+                        joined_data[[column]] <-
+                            zoo::na.fill(
+                                zoo::na.approx(joined_data[[column]], joined_data[[key]], na.rm = FALSE),
+                                "extend"
+                            )
                     }
                 }
             }
