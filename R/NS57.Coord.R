73da913be17700a6d2e83d93d12496de8abf2471
Zachary Burnett <zrb@umd.edu>
2017-05-16 22:40:53 -0400

    updated example datasets

---
+++ R/NS57.Coord.R
@@ -0,0 +1,4 @@
+#' LINK-TLM Data
+#'
+#' Flight NS57, 2016-07-30: Location data from received APRS packets.
+"NS57.Coord"
--- R/NS57_LINK_TLM.R
+++
@@ -1,4 +0,0 @@
-#' LINK-TLM Data
-#'
-#' Dataset containing location data from the main command payload of balloon flight NS57.
-"NS57_LINK_TLM"
