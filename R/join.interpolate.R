8010446d8c78fb01755320d3ec7ac2a21531efd6
Zachary Burnett <zrb@umd.edu>
2017-05-16 22:45:25 -0400

    updated interpolation function to default to keys

---
+++ R/join.interpolate.R
@@ -0,0 +1,47 @@
+#' Join datasets and interpolate missing fields
+#'
+#' Merges datasets, for instance IRENE and LINK-TLM
+#' @param data_1 First dataset.
+#' @param data_2 Second dataset.
+#' @param by One or more key with which to join datasets, ordered by priority.
+#' @export
+#' @importFrom zoo na.approx
+#' @importFrom zoo na.fill
+#' @examples
+#' interpolated_dataset <- join.interpolate(NS57.Coord, NS57.Rad)
+#'
+
+join.interpolate <-
+  function(data_1,
+           data_2,
+           by = c("DateTime", "Data_Source", "Flight"))
+  {
+    # outer join tables by key
+    joined_data <-
+      merge(
+        x = data_1,
+        y = data_2,
+        by = c(by),
+        all = TRUE
+      )
+
+    # replace NA values with interpolated values using zoo package
+    if (any(is.na(joined_data)))
+    {
+      data_types <- lapply(joined_data, class)
+
+      for (column in colnames(joined_data))
+      {
+        if ("numeric" %in% data_types[[column]] |
+            "integer" %in% data_types[[column]] &
+            !(column %in% by))
+        {
+          joined_data[[column]] <-
+            zoo::na.fill(zoo::na.approx(joined_data[[column]], joined_data[[by[1]]], na.rm = FALSE),
+                         "extend")
+        }
+      }
+    }
+
+    return(joined_data)
+  }
